d943fb45165fb46995f59f0d7ed9c0cf077e7da2
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-08 12:09:23 +0530

    created own observable of interval with next, complete, error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, observable } from 'rxjs';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    // custom Interval Observable
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        if(count===2){
+          observer.complete()
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'))
+        }
+        observer.next(count)
+        count++;
+      }, 1000)
+    })
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message)
+    }, ()=>{
+      console.log('Completed');
+    })
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
